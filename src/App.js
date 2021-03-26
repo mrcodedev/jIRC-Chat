ce228351df4b6388d389d1b967f4d7c30efe17a7
José Luis García <joseluis.garcia@altafonte.com>
2021-03-26 23:34:47 +0100

    feat(app): deleted app init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import { Host } from 'react'
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment className="App">
+      LA HISTORIA INTERMINAAAAABLEEEE
+    </React.Fragment>
   );
 }
 
